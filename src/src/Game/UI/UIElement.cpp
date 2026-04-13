53b33da5610c9fe723738279c6b843cab81c0ee6
Emil Franczak <251511@edu.p.lodz.pl>
2026-04-13 08:44:51 +0200

    Setters and Getters for UIElement

---
+++ src/src/Game/UI/UIElement.cpp
@@ -0,0 +1,17 @@
+#include "include/Game/UI/UIElement.hpp"
+
+void UIElement::SetVisible(bool value) {
+	isVisible = value;
+}
+
+bool UIElement::GetVisible() const {
+	return isVisible;
+}
+
+void UIElement::SetAlpha(float value) {
+	alpha = value;
+}
+
+float UIElement::GetAlpha() const {
+	return alpha;
+}
