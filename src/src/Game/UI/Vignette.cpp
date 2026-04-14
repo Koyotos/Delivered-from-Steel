942523428f35b9a5cd8a641481a3c0dae2601b2f
Emil Franczak <251511@edu.p.lodz.pl>
2026-04-14 20:24:00 +0200

    changing vignette state for testing purposes

--- src/src/Game/UI/Vignette.cpp
+++ src/src/Game/UI/Vignette.cpp
@@ -1,18 +1,21 @@
 #include "include/Game/UI/Vignette.hpp"
 #include "include/Renderer/Sprite.hpp"
+#include "include/Globals/Globals.hpp"
+#include <GLFW/glfw3.h>
+
 
 
 Vignette::Vignette(const std::unordered_map<std::string, std::any>& data) : UIElement(data) {
 	// states = fromMap(float, "states", data); i dont know why this line crashes, investigate later
 
-	states = 2;
-	currentValue = 0;
+	states = 3;
+	currentValue = 3.0f;
 	minValue = 0;
-	maxValue = 0;
+	maxValue = 5;
 }
 
 void Vignette::Draw() {
-	// SetCurrentSprite();
+	SetCurrentSprite();
 
 	// make better later
 	shader->SetMat4("VP", glm::ortho(0.0f, 1920.0f, 1080.0f, 0.0f, -1.0f, 1.0f));
@@ -24,6 +27,16 @@ void Vignette::SetCurrentSprite() {
 }
 
 void Vignette::Process() {
-	return;
+
+	if (Globals::GetGlobals().GetKeyState(GLFW_KEY_O)) {
+		currentValue -= 0.01f;
+	}
+
+	if (Globals::GetGlobals().GetKeyState(GLFW_KEY_P)) {
+		currentValue += 0.01f;
+	}
+
+	if (currentValue < minValue) currentValue = minValue;
+	if (currentValue > maxValue) currentValue = maxValue;
 }
 
