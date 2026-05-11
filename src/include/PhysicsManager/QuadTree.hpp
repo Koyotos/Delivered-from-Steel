28e759c877cf04230c8c7f8f7849a1f13c91174c
Emil Franczak <251511@edu.p.lodz.pl>
2026-05-11 21:46:11 +0200

    QuadTree class structure

---
+++ src/include/PhysicsManager/QuadTree.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+#include <array>
+
+#include "include/PhysicsManager/AABB.hpp"
+
+class PhysicsNode;
+
+class QuadTree
+{
+private:
+
+    static constexpr int MAX_OBJECTS = 4;
+    static constexpr int MAX_LEVELS = 5;
+
+    int level;
+
+    AABB bounds;
+
+    std::vector<std::shared_ptr<PhysicsNode>> objects;
+
+    std::array<std::unique_ptr<QuadTree>, 4> children;
+
+public:
+
+    QuadTree(int level, const AABB& bounds);
+
+    void Clear();
+
+    void Insert(std::shared_ptr<PhysicsNode> node);
+
+    void Query(
+        const AABB& area,
+        std::vector<std::shared_ptr<PhysicsNode>>& result);
+
+private:
+
+    void Split();
+
+    int GetIndex(const AABB& rect);
+};
